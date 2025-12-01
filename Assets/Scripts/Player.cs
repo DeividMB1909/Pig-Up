701a2d4ed0ae5f112c3567b9761837699524e190
JooseF57 <2236000408@utna.edu.mx>
2025-12-01 09:51:26 -0600

    Nivel de arena medianamente terminado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,88 +1,176 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movimiento")]
     public float speed = 5;
-    private Rigidbody2D rb2D;
-    private float move;
     public float jumpForce = 4;
-    private bool isGrounded;
+
+    [Header("Detección de Suelo")]
     public Transform groundCheck;
     public float groundRadius = 0.1f;
     public LayerMask groundLayer;
 
-    private Animator animator;
-
-    private int coins;
+    [Header("UI y Sistema")]
     public TMP_Text textCoins;
+    public float tiempoInvulnerable = 1.5f;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip coinClip;
+    public AudioClip barrelClip;
+
+    private Rigidbody2D rb2D;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private SistemaVidas sistemaVidas;
+
+    private float move;
+    private bool isGrounded;
+    private int coins;
+    private bool esInvulnerable = false;
+    private bool estaMuerto = false;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        sistemaVidas = FindObjectOfType<SistemaVidas>();
+
+        rb2D.interpolation = RigidbodyInterpolation2D.Interpolate;
+        ActualizarUI();
     }
 
     void Update()
     {
-        // Movimiento horizontal
+        if (estaMuerto) return;
+
         move = Input.GetAxis("Horizontal");
-        rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
 
-        // Voltear sprite
         if (move != 0)
-        {
             transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
-        }
 
-        // SALTO
         if (Input.GetButtonDown("Jump") && isGrounded)
-        {
             rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
 
-        }
-
-        // Par�metros del animator
         animator.SetFloat("Speed", Mathf.Abs(move));
         animator.SetFloat("VerticalVelocity", rb2D.linearVelocity.y);
         animator.SetBool("IsGrounded", isGrounded);
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
+        if (estaMuerto)
+        {
+            rb2D.linearVelocity = Vector2.zero;
+            return;
+        }
+
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+        rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void RecibirDaño()
     {
-        if (collision.CompareTag("Coin"))
+        if (estaMuerto || esInvulnerable) return;
+
+        sistemaVidas.PerderVida();
+        ActualizarUI();
+
+        if (sistemaVidas.vidasActuales <= 0)
         {
-            Destroy(collision.gameObject);
-            coins++;
+            Morir();
+        }
+        else
+        {
+            animator.SetTrigger("Hit");
+            StartCoroutine(InvulnerabilidadTemporal());
+        }
+    }
+
+    void Morir()
+    {
+        estaMuerto = true;
+
+        rb2D.linearVelocity = Vector2.zero;
+        rb2D.bodyType = RigidbodyType2D.Kinematic;
+
+        foreach (var col in GetComponents<Collider2D>())
+            col.enabled = false;
+
+        animator.SetTrigger("Die");
+        Invoke("ReiniciarEscena", 1.2f);
+    }
+
+    IEnumerator InvulnerabilidadTemporal()
+    {
+        esInvulnerable = true;
+
+        for (float t = 0; t < tiempoInvulnerable; t += 0.1f)
+        {
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        spriteRenderer.enabled = true;
+        esInvulnerable = false;
+    }
+
+    void ActualizarUI()
+    {
+        if (textCoins != null)
             textCoins.text = coins.ToString();
-        }
+    }
 
-        if (collision.transform.CompareTag("Spikes"))
+    void ReiniciarEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Monedas
+        if (other.CompareTag("Coin"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            coins++;
+            ActualizarUI();
+            audioSource.PlayOneShot(coinClip);
+            Destroy(other.gameObject);
         }
 
-        if(collision.transform.CompareTag("Barrel"))
+        // Pinchos
+        if (other.CompareTag("Spikes"))
         {
-            Vector2 knockbackDir = (rb2D.position - (Vector2)collision.transform.position).normalized;
-            rb2D.linearVelocity = Vector2.zero;
-            rb2D.AddForce(knockbackDir * 3, ForceMode2D.Impulse);
-
-            BoxCollider2D[] colliders = collision.gameObject.GetComponents<BoxCollider2D>();
-
-            foreach (BoxCollider2D col in colliders)
-            {
-                col.enabled = false;
-            }
-
-            collision.GetComponent<Animator>().enabled=true;
-            Destroy(collision.gameObject, 0.5f);
+            RecibirDaño();
         }
+
+        // Barriles Restaurado
+        if (other.CompareTag("Barrel"))
+        {
+            audioSource.PlayOneShot(barrelClip);
+
+            Animator barrelAnimator = other.GetComponent<Animator>();
+            if (barrelAnimator != null)
+                barrelAnimator.enabled = true;
+
+            // Rebote / knockback pequeño
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, 2f);
+
+            // Quitar colisiones
+            foreach (var c in other.GetComponents<Collider2D>())
+                c.enabled = false;
+
+            Destroy(other.gameObject, 0.4f);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
     }
 }
